2f56586ac16bb5808f39673463c66bd1609dac7a
liubida <d@sendcloud.im>
2016-11-11 10:20:53 +0800

    backup.sh

---
+++ backup.sh
@@ -0,0 +1,12 @@
+
+
+
+mkdocs build --clean
+rm -f doc/index.html
+cp -f index.html doc/
+rm -rf doc/test
+cp -a test_v2 doc/test
+cp -a doc/email/*  doc/email_temp/
+
+#cp -a doc/email_temp/*  doc/email/
+
