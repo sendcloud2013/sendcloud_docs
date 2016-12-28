8adfa72d2eccce778597d07b98a8c2f043655358
liubida <d@sendcloud.im>
2016-12-28 10:35:18 +0800

    dsaf

--- backup.sh
+++ backup.sh
@@ -6,9 +6,9 @@ rm -f doc/index.html
 cp -f index.html doc/
 rm -rf doc/test
 cp -a test_v2 doc/test
-#cp -a doc/email/*  email_temp/
+cp -a doc/email/*  email_temp/
 
 
-cp -a email_temp/*  doc/email/
-rm -rf email_temp/*
+#cp -a email_temp/*  doc/email/
+#rm -rf email_temp/*
 
